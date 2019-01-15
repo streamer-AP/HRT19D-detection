3352cffd48d6f6ffa9b766f1e8323a7fd0f8d46b
Yan <cryanxin@foxmail.com>
2019-01-15 11:16:42 +0800

    代码注释及实验说明

--- Python/image/arctest.py
+++ Python/image/arctest.py
@@ -2,7 +2,11 @@
 =======
 Arctest
 =======
-
+用来绘制函数 exp(-t)*cos(2pi*t)的图像
+其中 plt.plot 用于具体绘制，‘bo’ 为线形。表示蓝色圆圈。
+plt.setp 可以对列表或单个对象进行属性设置
+markersize 设置圆圈大小
+markerfacecolor 设置圆圈的前景色
 """
 import matplotlib.pyplot as plt
 import numpy as np
@@ -17,8 +21,8 @@ def f(t):
 
 t1 = np.arange(0.0, 5.0, .2)
 
-l = plt.plot(t1, f(t1), 'ro')
-plt.setp(l, markersize=30)
-plt.setp(l, markerfacecolor='C0')
+l = plt.plot(t1, f(t1), 'bo')
+plt.setp(l, markersize=10)
+plt.setp(l, markerfacecolor='#000000')
 
 plt.show()
--- Python/image/bar_stacked.py
+++ Python/image/bar_stacked.py
@@ -7,7 +7,10 @@ This is an example of creating a stacked bar plot with error bars
 using `~matplotlib.pyplot.bar`.  Note the parameters *yerr* used for
 error bars, and *bottom* to stack the women's bars on top of the men's
 bars.
-
+柱状图的绘制演示，程序首先定义了男性，女性在随年龄值变化时的某项数值得分的均值和方差。
+plt.bar 函数前三个位置参数为 横轴，纵轴及柱宽度。
+yerr 指y 方向的错误柱
+bottom 为柱的底面起始高度。
 """
 
 import numpy as np
