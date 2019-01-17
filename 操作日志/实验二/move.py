8d93919667859c8a06615bf3638f29bae80aab17
Bean <xvbean123@163.com>
2019-01-17 08:15:27 +0800

    实验二

---
+++ 操作日志/实验二/move.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+import roslib; roslib.load_manifest('teleop_twist_keyboard')
+import rospy
+
+from geometry_msgs.msg import Twist
+
+import sys, select, termios, tty
+
+msg = """
+Moving around:
+   w a s d
+"""
+moveBindings = {
+		'a':(0,0,0,1),
+		'd':(0,0,0,-1),
+		's':(-1,0,0,0),
+		'w':(1,0,0,0),
+	       }
+def getKey():
+	tty.setraw(sys.stdin.fileno())
+	select.select([sys.stdin], [], [], 0)
+	key = sys.stdin.read(1)
+	termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+	return key
+
+
+if __name__=="__main__":
+    	settings = termios.tcgetattr(sys.stdin)
+	
+	pub = rospy.Publisher('cmd_vel', Twist, queue_size = 1)
+	rospy.init_node('move')
+
+	speed = rospy.get_param("~speed", 1.0)
+	turn = rospy.get_param("~turn", 1.0)
+	x = 0
+	y = 0
+	z = 0
+	th = 0
+	status = 0
+
+	try:
+		print msg
+		while(1):
+			key = getKey()
+			if key in moveBindings.keys():
+				x = moveBindings[key][0]
+				y = moveBindings[key][1]
+				z = moveBindings[key][2]
+				th = moveBindings[key][3]
+			else:
+				x = 0
+				y = 0
+				z = 0
+				th = 0
+				if (key == '#'):
+					break
+
+			twist = Twist()
+			twist.linear.x = x*speed; twist.linear.y = y*speed; twist.linear.z = z*speed;
+			twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = th*turn
+			pub.publish(twist)
+
+	except:
+		print e
+
+	finally:
+		twist = Twist()
+		twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0
+		twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
+		pub.publish(twist)
+
+    		termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+
